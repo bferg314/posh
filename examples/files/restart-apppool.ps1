97f78edfe88a6bd39a3b53a8a1cb193f9fdd4b0a
bferg314 <ninjabreath@gmail.com>
2022-11-02 14:34:33 -0400

    Added exit codes

--- examples/files/restart-apppool.ps1
+++ examples/files/restart-apppool.ps1
@@ -9,6 +9,7 @@ $WebAdminModule = "WebAdministration"
 try {
     if (-not(get-module -listavailable | where-object { $_.Name -eq $WebAdminModule })) {
         write-host "$WebAdminModule Module not available, exiting..." -foreground red
+        exit 1
     }
     else {
         write-host ">> Attempting to restart My Website..." -foreground cyan
@@ -20,15 +21,17 @@ try {
             Write-Host "$AppPoolName is currently $poolState"
             restart-webapppool $AppPoolName
             Write-Host "$AppPoolName was succesfully restarted"
+            exit 0
         }
         else {
             Write-Host "$AppPoolName is unavailable"
-            exit 1
+            exit 2
         }
     }
 }
 catch {
     Write-Host "Exception :: $_"
+    exit 99
 }
 
 
