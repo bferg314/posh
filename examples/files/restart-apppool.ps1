ea4a070db20820594a02044fdc49cc1ed7086fb4
bferg314 <ninjabreath@gmail.com>
2022-11-02 14:30:55 -0400

    fixed bad check for module

--- examples/files/restart-apppool.ps1
+++ examples/files/restart-apppool.ps1
@@ -3,32 +3,28 @@ param (
     [string]$AppPoolName = ""
 )
 
-$ModuleName = "WebAdministration"
+# $ModuleName = "IISAdministration"
+$WebAdminModule = "WebAdministration"
 
 try {
-
-    if (get-module -name $ModuleName) {
-        if (-not(get-module -listavailable | where-object { $_.Name -eq $ModuleName })) {
-            write-host "$ModuleName Module not available, exiting..." -foreground red
+    if (-not(get-module -listavailable | where-object { $_.Name -eq $WebAdminModule })) {
+        write-host "$WebAdminModule Module not available, exiting..." -foreground red
+    }
+    else {
+        write-host ">> Attempting to restart My Website..." -foreground cyan
+        write-host ""
+        
+        import-module $WebAdminModule
+        $poolState = (Get-WebAppPoolState $AppPoolName).value
+        if ($poolState) {
+            Write-Host "$AppPoolName is currently $poolState"
+            restart-webapppool $AppPoolName
+            Write-Host "$AppPoolName was succesfully restarted"
         }
         else {
-            write-host ">> Attempting to restart My Website..." -foreground cyan
-            write-host ""
-        
-            import-module $ModuleName
-            $poolState = (Get-WebAppPoolState $AppPoolName).value
-            if ($poolState) {
-                Write-Host "$AppPoolName is currently $poolState"
-                restart-webapppool $AppPoolName
-                Write-Host "$AppPoolName was succesfully restarted"
-            }
-            else {
-                Write-Host "$AppPoolName is unavailable"
-                exit 1
-            }
+            Write-Host "$AppPoolName is unavailable"
+            exit 1
         }
-    } else {
-        Write-Host "Unable to find Module $ModuleName"
     }
 }
 catch {
